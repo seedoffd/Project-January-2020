4046fbba8743b3bb9d58eff96439dc449a2076a3
seedoffd <seedoffd@gmail.com>
2020-01-17 17:16:17 -0600

    bastion updated

--- key.tf
+++ key.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "terraform" { 
-  key_name   = "terraform" 
+resource "aws_key_pair" "syed-bastion" { 
+  key_name   = "syed-bastion" 
   public_key = file("~/.ssh/id_rsa.pub")
 }
